a68f146b7d89b21d59d24d53f928ac3166bd1388
JosinBahaswan <josinbahaswan@gmail.com>
2025-06-23 12:27:27 +0700

    try to connect railway

--- main.py
+++ main.py
@@ -115,3 +115,8 @@ def save_table_image(image_base64: str):
         return {"message": "Gambar tabel berhasil disimpan.", "filename": filename}
     except Exception as e:
         return {"error": str(e)}
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
